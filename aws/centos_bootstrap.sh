185ef6cf38f91bbedafe6f433a62a97060b79bc0
mortonjt <jamietmorton@gmail.com>
2020-03-22 18:05:11 -0400

    adding TODO

--- aws/centos_bootstrap.sh
+++ aws/centos_bootstrap.sh
@@ -4,4 +4,8 @@ virtualenv roberta -p python3.6
 source /home/ec2-user/roberta/bin/activate
 # TODO: what about GPU drivers and NCCL??
 pip install torch==1.4
-pip install git+git@github.com:mortonjt/fairseq.git
+# TODO: the gcc version is grossly out of date (it should be version 8.3 not 4.8)
+# TODO: put exact version numbers of all of the packages
+pip install numpy scipy
+pip install tensorboardX
+pip install git+https://github.com/pytorch/fairseq.git
